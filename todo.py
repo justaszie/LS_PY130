03c5307295461e98086ca476c3205aec6b9d475b
Justas Zieminykas <justaxsz@gmail.com>
2024-10-05 09:32:27 +0300

    Added higher order functions to the TodoList class to make it more generic

--- todo.py
+++ todo.py
@@ -72,17 +72,93 @@ class TodoList:
         self.todo_at(idx).done = False
 
     def mark_all_done(self):
-        # On purpose not accessing the underlying list directly
-        # Going through the public methods instead for better abstraction
-        for idx in range(len(self)):
-            self.mark_done_at(idx)
+        def mark_done(todo):
+            todo.done = True
+
+        self.each(mark_done)
 
     def mark_all_undone(self):
-        for idx in range(len(self)):
-            self.mark_undone_at(idx)
+        def mark_undone(todo):
+            todo.done = False
+
+        self.each(mark_undone)
 
     def all_done(self):
         return all(todo.done for todo in self._todos)
 
     def remove_at(self, idx):
         del self._todos[idx]
+
+    def each(self, callback):
+        for todo in self._todos:
+            callback(todo)
+
+    def select(self, callback):
+        new_list = TodoList(self.title)
+        for todo in filter(callback, self._todos):
+            new_list.add(todo)
+
+        return new_list
+
+    def find_by_title(self, title):
+        found = self.select(lambda todo: todo.title == title)
+        return found.first()
+
+    def done_todos(self):
+        return self.select(lambda todo: todo.done)
+
+    def undone_todos(self):
+        return self.select(lambda todo: not todo.done)
+
+    def mark_done(self, title):
+        todo = self.find_by_title(title)
+        todo.done = True
+
+
+
+
+
+empty_todo_list = TodoList('Nothing Doing')
+
+def setup():
+
+    todo1 = Todo('Buy milk')
+    todo2 = Todo('Clean room')
+    todo3 = Todo('Go to gym')
+
+    todo2.done = True
+
+    todo_list = TodoList("Today's Todos")
+    todo_list.add(todo1)
+    todo_list.add(todo2)
+    todo_list.add(todo3)
+
+    return todo_list
+
+
+def step_8():
+    print('--------------------------------- Step 8')
+    todo_list = setup()
+
+    print(todo_list)
+    # ---- Today's Todos -----
+    # [ ] Buy milk
+    # [X] Clean room
+    # [ ] Go to gym
+
+    todo_list.mark_all_done()
+    print(todo_list)
+    # ---- Today's Todos -----
+    # [X] Buy milk
+    # [X] Clean room
+    # [X] Go to gym
+
+    todo_list.mark_all_undone()
+    print(todo_list)
+    # ---- Today's Todos -----
+    # [ ] Buy milk
+    # [ ] Clean room
+    # [ ] Go to gym
+
+step_8()
+
